e0c5a9c25446f5e88fed30edd37c4c45a701688f
Sébastien <seb.boukobza@gmail.com>
2024-05-03 16:30:33 +0200

    beaucoup

---
+++ html-tp1/footer.php
@@ -0,0 +1,16 @@
+<div class=footers>
+    <ul class=contact>
+        <li id=f2>
+            <h4>Frouzins</h4>
+            <p>factaekwondo@gmail.com<br>10 Avenue de Gascogne, 31270 Frouzins</p>
+        </li>
+        <li id=f1>
+            <h2>Contacts</h2>
+            <p>Fanny Burguion : 0636552782 <br>burguion.fanny@me.com</p>
+        </li>
+        <li id=f3>
+            <h4>Tournefeuille</h4>
+            <p>burguion.fanny@me.com<br>78 Avenue du Général de Gaulle, 31170 Tournefeuille</p>
+        </li>
+    </ul>
+</div>
